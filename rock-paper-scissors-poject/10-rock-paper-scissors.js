1e893b77383464e17b3d47eb3813a730c91c841d
AbdellahElasy <abdellahelasry97@gmail.com>
2025-07-24 18:11:00 +0100

    Auto Play button

--- rock-paper-scissors-poject/10-rock-paper-scissors.js
+++ rock-paper-scissors-poject/10-rock-paper-scissors.js
@@ -12,6 +12,25 @@ UpdateScoreElement();
 
        
       /* add a function */
+      let isAutoPlaying = false;
+let intervalId;
+function autoPlay(){
+  if(!isAutoPlaying){
+  intervalId =  setInterval(function(){
+   const playerMove = pickComputerMove();
+   playGame(playerMove);
+  }, 1000);
+  isAutoPlaying = true;
+
+
+  }else{
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+}
+
+
+
       function playGame(playerMove){
         const computerMove =   pickComputerMove();
      result = '';
